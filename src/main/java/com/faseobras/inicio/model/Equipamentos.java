4b54af4f8909189a0c2090418d1b6d92d31dbea0
Marcius Junior <marcius.junior@hotmail.com>
2021-10-01 15:30:12 -0300

    API 100% COMPLETA

--- src/main/java/com/faseobras/inicio/model/Equipamentos.java
+++ src/main/java/com/faseobras/inicio/model/Equipamentos.java
@@ -7,7 +7,9 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import com.faseobras.inicio.model.dto.FaseDeObrasDTO;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.Data;
 import lombok.Getter;
@@ -24,8 +26,8 @@ public class Equipamentos {
         
         private String equipamento;
 
-        
-        @DBRef
+        @JsonIgnoreProperties("equipamentos")
+        @DBRef(lazy = true)
         private List<FaseDeObras> faseDeObras = new ArrayList<>();
         
 
--- src/main/java/com/faseobras/inicio/model/FaseDeObras.java
+++ src/main/java/com/faseobras/inicio/model/FaseDeObras.java
@@ -8,6 +8,9 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
@@ -23,7 +26,7 @@ public class FaseDeObras {
         
         private String nomeDaFase;
 
-        
+        @JsonIgnoreProperties("faseDeObras")
         @DBRef
         private List<Equipamentos> equipamentos = new ArrayList<>();
 
